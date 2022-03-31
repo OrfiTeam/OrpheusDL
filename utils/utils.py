fa5963157ac97d89cd7691584c65dbfbdb3fe1bf
Dniel97 <6324072+Dniel97@users.noreply.github.com>
2022-04-01 01:44:48 +0200

    Hotfix: removes any trailing whites paces

--- utils/utils.py
+++ utils/utils.py
@@ -19,7 +19,7 @@ def create_requests_session():
     session_.mount('https://', HTTPAdapter(max_retries=retries))
     return session_
 
-sanitise_name = lambda name : re.sub(r'[:]', ' - ', re.sub(r'[\\\/*?"<>|$]', '', re.sub(r'[ \t]+$', '', str(name))))
+sanitise_name = lambda name : re.sub(r'[:]', ' - ', re.sub(r'[\\\/*?"<>|$]', '', re.sub(r'[ \t]+$', '', str(name).rstrip())))
 
 
 def fix_file_limit(path: str, file_limit=250):
